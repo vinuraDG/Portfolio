baf5717352786b284a9f1a4f829366d948cefc3b
D.G.V.Deelaka <vinuragamage123@gmail.com>
2025-10-22 19:19:29 +0530

    complete the portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return(
         <div className="py-8 bg-black/20">
             <div className="container mx-auto px-6 text-center">
-                <p className="text-gray-300">@ 2025 Created Ny Vinura Deelaka.All rights reserved.</p>
+                <p className="text-gray-300">@ 2025 Created By Vinura Deelaka.All Rights Reserved.</p>
             </div>
             
         </div>
